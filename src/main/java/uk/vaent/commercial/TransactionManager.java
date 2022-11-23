a6e0981171f75c4388c51b22743ca5bc41c57b26
Vaent <gh@vaent.uk>
2022-11-23 21:56:38 +0000

    Set up Java project
    Added pom.xml with JUnit dependency.
    
    Added base class with public methods stubbed.

---
+++ src/main/java/uk/vaent/commercial/TransactionManager.java
@@ -0,0 +1,15 @@
+package uk.vaent.commercial;
+
+public class TransactionManager {
+    public int scan(char itemCode) {
+        return 0;
+    }
+
+    public int checkout() {
+        return 0;
+    }
+
+    public boolean pay(int payment) {
+        return true;
+    }
+}
---
+++ src/test/java/uk/vaent/commercial/TransactionManagerTest.java
@@ -0,0 +1,7 @@
+package uk.vaent.commercial;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TransactionManagerTest {
+
+}
