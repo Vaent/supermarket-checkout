35fb4e0651f9e26de7114558ef21763e6804aee6
Vaent <gh@vaent.uk>
2022-11-24 19:36:09 +0000

    Refactor CliApp
    Moved code from main method into CliApp instance and extracted some code
    into fields and more focused methods.

--- src/main/java/uk/vaent/commercial/CliApp.java
+++ src/main/java/uk/vaent/commercial/CliApp.java
@@ -3,35 +3,49 @@ package uk.vaent.commercial;
 import java.util.Scanner;
 
 public class CliApp {
+    private Scanner scanner = new Scanner(System.in);
+    private TransactionManager transactionManager = new TransactionManagerImpl();
+    private String validItemsPattern = "^[A-D]$";
+
     public static void main(String... args) {
-        TransactionManager transactionManager = new TransactionManagerImpl();
-        Scanner scanner = new Scanner(System.in);
+        new CliApp().run();
+    }
+
+    private void run() {
         System.out.println();
         System.out.println("Enter the word 'quit' at any time to exit the app.");
-        do {
-            System.out.println();
-            System.out.println("Enter the word 'checkout' to view the total cost of this transaction.");
-            System.out.println("Enter the word 'pay' to finish this transaction");
-            System.out.println("Enter a character (A, B, C or D) to scan a new item");
-            System.out.print("Your selection: ");
+        while (true) {
+            promptUserForInput();
             String input = scanner.nextLine().strip();
             if (input.isEmpty()) continue;
             if (input.equalsIgnoreCase("quit")) return;
-            if (input.equalsIgnoreCase("checkout")) {
-                System.out.println("The current subtotal is " + transactionManager.checkout());
-            } else if (input.equalsIgnoreCase("pay")) {
-                if (transactionManager.pay()) {
-                    System.out.println("The transaction has been paid");
-                } else {
-                    System.out.println("There was a problem with the payment");
-                }
-            } else if (input.matches("^[A-D]$")) {
-                int subtotal = transactionManager.scan(input.charAt(0));
-                System.out.println("The current subtotal is " + subtotal);
+            handleTransactionCommand(input);
+        }
+    }
+
+    private void handleTransactionCommand(String input) {
+        if (input.equalsIgnoreCase("checkout")) {
+            System.out.println("The current subtotal is " + transactionManager.checkout());
+        } else if (input.equalsIgnoreCase("pay")) {
+            if (transactionManager.pay()) {
+                System.out.println("The transaction has been paid");
             } else {
-                System.out.println("Input not recognised.");
-                if (input.matches("^[a-d]$")) System.out.println("Make sure you type the exact character (upper/lower case).");
+                System.out.println("There was a problem with the payment");
             }
-        } while (true);
+        } else if (input.matches(validItemsPattern)) {
+            int subtotal = transactionManager.scan(input.charAt(0));
+            System.out.println("The current subtotal is " + subtotal);
+        } else {
+            System.out.println("Input not recognised.");
+            if (input.matches(validItemsPattern.toLowerCase())) System.out.println("Make sure you type the exact character (upper/lower case).");
+        }
+    }
+
+    private void promptUserForInput() {
+        System.out.println();
+        System.out.println("Enter the word 'checkout' to view the total cost of this transaction.");
+        System.out.println("Enter the word 'pay' to finish this transaction");
+        System.out.println("Enter a character (A, B, C or D) to scan a new item");
+        System.out.print("Your selection: ");
     }
 }
